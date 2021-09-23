af95ef662708c9a6152f018150e679e64440fbd0
ridia13 <ridia2005@naver.com>
2021-09-23 22:19:45 +0900

    react-class ver

--- NumberBaseball.jsx
+++ NumberBaseball.jsx
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import Try from './try.jsx';
+import Try from './Try.jsx';
 let strike = 0;
 let ball = 0;
 let out = 0;
@@ -17,9 +17,9 @@ function  getNumbers() {//랜덤숫자 4개 겹치지않게 뽑기
 
 class NumberBaseball extends Component{
   state = {
+    answer: getNumbers(),//ex) [1,2,3,5]
     value: '',
     result: '',
-    answer: getNumbers(),//ex) [1,2,3,5]
     tries:[],
   };
 
@@ -32,99 +32,88 @@ class NumberBaseball extends Component{
     const {value} = this.state;
     const arrNum = value.split('');
     const zero = arrNum.includes("0");
-    const overlap = new Set(arrNum);
-  
+    const overlap = new Set(arrNum);//중복 자동제거
+    console.log(`arrNum: ${arrNum}`);
     if (zero || overlap.size < 4 || value.length < 4) { //숫자 올바른지 확인
       alert("중복되지 않는 숫자 4개를 입력해주세요. (범위: 1~9)");
     } else { //기존 숫자와의 중복 확인
-      checkPN(value);
+      this.checkPN();
     }
   }
 
-  // checkPN(value) { //기존 숫자와의 중복확인
-  //   const {tries,value} = this.state;
-  //   let overlapPN = false;
-  //   tries.forEach((v,i) => {
-  //     if(v.try === value){
-  //       overlapPN = true;
-  //     }
-  //   })
-  
-  //   if (overlapPN) {
-  //     alert("기존 숫자와 중복됩니다. 다시 입력해수세요.");
-  //   } else {
-      
-  //     checkResult(value);
-  //   }
-  // }
-  
-  // strikeAndBall(number) {
-    
-  //   const arrPick = number.split('');
-  //   const arrPN = arrPick.map((v) => Number(v));
-  
-  //   randomNums.forEach((v, i) => { //b,s 출력
-  //     if (arrPN.includes(v)) {
-  //       i === arrPN.indexOf(v) ? strike++ : ball++;
-  //     }
-  //   })
-  //   checkOut(number);
-  //   strike = 0;
-  //   ball = 0;
-  // }
+  checkOut(){//out 확인
+    const {result, value, answer, tries} = this.state;
+    const joinAnswer = answer.join('');
+    if (strike === 0 && ball === 0) { 
+      out++;
+      this.setState((prevState) => {
+        return{
+          tries: [...prevState.tries, {tryNum: value, result: `${out} OUT`}],
+        }
+      });
+      out === 3 ? this.setState({result:`You failed😓 정답 : ${joinAnswer}`}) : null;
+    }else{
+      this.setState((prevState) => {
+        return{
+          tries: [...prevState.tries, {tryNum: value, result: `${ball}B, ${strike}S`}],
+        }
+      });
+    }
+  }
 
-  //  checkResult(number) {//입력 결과 확인
-  //   const {answer, value, result, tries} = this.state;
-  //   const joinAnswer = answer.join('');
-  //   const br = document.createElement('br');
-  
-  //   if (joinAnswer === value) {//HR확인
-  //     this.setState({
-  //       result:`${joinAnswer} HomeRun🎉`,
-  //     })
-  //   } else if (tries.length >= 9) {//시도횟수 확인
-  //     this.setState({
-  //       result:`${value} : You failed😓 정답 : ${joinAnswer} `,
-  //     })
-  //   } else {//s,b 출력
-  //     this.setState((prevState) => {
-  //       tries:[...prevState.tries, {try: value, result: `${strike}S ${ball}B`}],
-  //     })
-  //     strikeAndBall(number);
-  //   }
-  // }
+  strikeAndBall(){
+    const {value, answer,tries} = this.state;
+    const arrPick = value.split('');
+    const arrPN = arrPick.map((v) => parseInt(v));
+
+    answer.forEach((v, i) => { //b,s 출력
+      if (arrPN.includes(v)) {
+        i === arrPN.indexOf(v) ? strike++ : ball++;
+      }
+    })
+    this.checkOut();
+  }
+
+  checkResult() {//입력 결과 확인
+    const {answer, value, result, tries} = this.state;
+    const joinAnswer = answer.join('');
+   
+    if (joinAnswer === value) {//HR확인
+      this.setState({
+        result:`${joinAnswer}: HomeRun🎉`,
+      })
+    } else if (tries.length >= 9) {//시도횟수 확인
+      this.setState({
+        result:`You failed😓 정답: ${joinAnswer} `,
+      })
+    }else {//s,b 출력
+      this.strikeAndBall();
+    }
+  }
+
+  checkPN() { //기존 숫자와의 중복확인
+    const {value, tries} = this.state;
+    let overlapPN = false;
+    tries.forEach((v, i) => {
+      if(v.tryNum === value){
+        overlapPN = true;
+      }
+    })
+
+    if (overlapPN) {
+      alert("기존 숫자와 중복됩니다. 다시 입력해수세요.");
+    } else {
+      this.checkResult();
+    }
+  }
+ 
   onSubmitForm = (e) => {
     e.preventDefault();
-    const {result, value, tries, answer} = this.state;//구조분해
-    const joinAnswer = answer.join('');
-    const input = e.target[0];
-    console.dir(e.target[0].value);
-    if(joinAnswer === value){//홈런
-      this.setState({
-        result:`${joinAnswer} HomeRun🎉`,
-      })
-    }else if(tries.length >= 9){//기회 모두 다 썼을 경우
-      this.setState({
-        result:'You failed😓 정답 :' + joinAnswer.join(','),
-      })
-    }else{//기회 9번 이하 경우
-      const arrValue = value.split('');
-      const arrPV = arrValue.map((v) => parseInt(v));
-      answer.forEach((v, i) => { //b,s 출력
-        if (arrPV.includes(v)) {
-          i === arrPV.indexOf(v) ? strike++ : ball++;
-          this.setState((prevState)=> {
-            return{
-              tries:[...prevState.tries, {try: value, result: `${strike}S ${ball}B`}],
-            }
-          })
-        }
-      })
-    }
-
-    strike = 0;
+    const {value} = this.state;
+    strike = 0;      
     ball = 0;
-    input.value = '';
+    this.rightNum();
+    this.setState({value: ''});
   }
 
   render(){
@@ -133,21 +122,20 @@ class NumberBaseball extends Component{
       <>
         <div>{result}</div>
         <form onSubmit={this.onSubmitForm}>
-          <input defaultValue={value} onChange={this.onChangeInput} type="text" maxLength="4" placeholder="Please enter 4 numbers"/>
+          <input value={value} onChange={this.onChangeInput} type="text" maxLength="4" placeholder="Please enter 4 numbers"/>
           <button>Submit</button>
         </form>
         <div>시도: {tries.length}</div>
         <ul>
             {tries.map((v,i) => {
               return(
-                <Try key={v.try} tryInfo= {v}/>               
+                <Try key={v.tryNum} v= {v}/>   
               )
             })}
         </ul>
       </>
     )
   }
-  
 }
 export default NumberBaseball;
 
--- Try.jsx
+++ Try.jsx
@@ -1,10 +1,19 @@
-import React from 'react';
-const Try = ({tryInfo}) =>{//{tryInfo}자리에 props 도 가능 -> return안에도 props 적어줘야함
-  return(
-    // props 존재 시 부모가 있다는 것, but 부모도 props라면 리덕스 등 추천
-    <li>
-      try: <b>{tryInfo.try}</b>, result: <b>{tryInfo.result}</b>
-    </li> 
-  )
+import React, {Component} from 'react';
+
+class Try extends Component{
+  render(){
+    const {v} = this.props;
+    return(
+      <li>
+        try: <b>{v.tryNum}</b>, result: <b>{v.result}</b>
+      </li>
+  )}
 }
 export default Try;
+
+// hooks
+// const Try = ({tryInfo}) =>{//{tryInfo}자리에 props 도 가능 -> return안에도 props 적어줘야함
+// props 존재 시 부모가 있다는 것, but 부모도 props라면 리덕스 등 추천
+// <li>
+//   try: <b>{tryInfo.try}</b>, result: <b>{tryInfo.result}</b>
+// </li> 
